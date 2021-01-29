296d8b00819c2a4bcc390508c307bf3325bcb74d
Otuekong Shield <myshield02@email.com>
2021-01-29 16:06:07 +0100

    Footer layout of the admin pages
    This page has content of the footer section of the admin
    This is the initial commit

---
+++ app/layouts/admin_footer.php
@@ -0,0 +1,10 @@
+<footer>
+
+        <div class="copyright">
+            <p><?php echo date('Y') ?> &copy; copyright PHP-MYSQL COURSE</p>
+        </div>
+    </footer>
+</body>
+</html>
+
+<?php db_disconnect($db_conn); ?>
