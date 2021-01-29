a3a657effe180e4ea4ddd7ba135b71d12e1f16f6
Otuekong Shield <myshield02@email.com>
2021-01-29 16:04:50 +0100

    Header section of admin pages
    This page has the header content of the admin pages.
    This is the initial commit.

---
+++ app/layouts/admin_header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin - <?php echo htmlspecialchars($page_title); ?></title>
+</head>
+<body>
+    <div>
+        <nav>
+            <li><a href="<?php echo admin_url('index.php'); ?>">Admin Home</a></li>
+        </nav>
+    </div>
