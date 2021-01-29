50d21c855b216d7370b5da2ed08f07cef5c80e79
Otuekong Shield <myshield02@email.com>
2021-01-29 16:03:41 +0100

    Index page of the application
    This is the initial commit of this page

---
+++ app/index.php
@@ -0,0 +1,18 @@
+<?php require_once('../scripts/initialize.php'); ?>
+
+<?php include(LAYOUT_PATH . '/header.php'); ?>
+
+    <div class="banner">
+        <h1>PHP-MYSQL COURSE</h1>
+        <p>A course on PHP and MYSQL Technique</p>
+    </div>
+
+    <div class="nav">
+        <?php include(LAYOUT_PATH . '/navigation.php'); ?>
+    </div>
+    
+    <div class="content">
+
+    </div>
+    
+<?php include(LAYOUT_PATH . '/footer.php'); ?>
