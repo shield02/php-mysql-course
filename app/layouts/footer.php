16f4cbfbe3871218a47d271e55fb35a8b54a132c
Otuekong Shield <myshield02@email.com>
2021-02-05 11:11:29 +0100

    Public footer
    Has all the codes regarding the footer section of the public domain. Including
    closing any database connection.

---
+++ app/layouts/footer.php
@@ -0,0 +1,9 @@
+<footer>
+        <div class="copyright">
+            <p><?php echo date('Y') ?> &copy; copyright</p>
+        </div>
+    </footer>
+</body>
+</html>
+
+<?php db_disconnect($db_conn); ?>
